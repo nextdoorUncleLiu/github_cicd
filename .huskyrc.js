81f1268956509cc2ab9ed5f4df865b5cc71f4c09
liuxichen <liuxichen@360.cn>
2020-09-04 11:12:00 +0800

    同步测试

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,5 +1,10 @@
+const { exec } = require('child_process')
+exec('git rev-parse --abbrev-ref HEAD', (error, stdout, stderr) => {
+  console.log(error, stdout, stderr)
+})
+console.log()
 module.exports = {
   'hooks': {
-    'pre-commit': 'npm run build && git add build '
+    'pre-push': 'npm run build'
   }
 }
