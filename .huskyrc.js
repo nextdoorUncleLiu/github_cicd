89168195d9b4429e0be808ac6028db4ca43bf94b
liuxichen <liuxichen@360.cn>
2020-09-04 11:27:50 +0800

    同步测试

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,5 +1,5 @@
 const { execSync } = require('child_process')
-
+console.log(2)
 const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().replace(/\s+/, '')
 console.log(branch)
 console.log(1)
