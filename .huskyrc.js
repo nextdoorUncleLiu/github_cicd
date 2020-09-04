54d57100f8d01c5d53810f5d43a120367763b2ef
liuxichen <liuxichen@360.cn>
2020-09-04 11:26:25 +0800

    同步测试

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,6 +2,7 @@ const { execSync } = require('child_process')
 
 const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().replace(/\s+/, '')
 console.log(branch)
+console.log(1)
 module.exports = {
   'hooks': {
     // 'pre-push': 'npm run build'
